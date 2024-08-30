ce0090554edbf5e460f556243cfe60101cb5cf92
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-30 13:13:36 +0430

    🏘📊💥 implement Home page / Sidenav / Mobile Sidenav / mobile responsive / Doughnut Chart 💪😎

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const LoggedIn = { lastName: "Fateh" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and manage your account and transactions efficiently"
+            user={LoggedIn.lastName}
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1353.23}
+          />
+        </header>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
